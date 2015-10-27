44146e051883058428528a5087bcf6db5a4a1188
alshin <dm.alshin@gmail.com>
2015-10-27 15:46:11 +0300

    Основной код компонента

---
+++ lang/en/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['IBLOCK_ELEMENT_ADD_FORM_NAME'] = "Multiply creation form";
+$MESS ['IBLOCK_ELEMENT_ADD_FORM_DESCRIPTION'] = "Multiply information block elemenst creation form";
+$MESS ['T_IBLOCK_DESC_ELEMENT_ADD'] = "Add elements";
+?>
---
+++ lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['IBLOCK_ELEMENT_ADD_FORM_NAME'] = "Форма добавления нескольких элементов";
+$MESS ['IBLOCK_ELEMENT_ADD_FORM_DESCRIPTION'] = "Форма добавления нескольких элементов инфоблока";
+$MESS ['T_IBLOCK_DESC_ELEMENT_ADD'] = "Добавление элементов";
+?>
